9ab2b7311be3a999085b23a22e4504a8cdaf5a8b
jzm-chairman <37035235+jzm-chairman@users.noreply.github.com>
2020-05-27 02:44:19 +0800

    beutify the frontend

--- sources/SearchEngine-FrontEnd/src/common/Header.vue
+++ sources/SearchEngine-FrontEnd/src/common/Header.vue
@@ -1,3 +1,17 @@
 <template>
-    <div class="header">几把搜索(JB Search)</div>
+    <div class="header">
+        <img src="../assets/logo.jpg" class="logo">
+        几把搜索(JB Search)
+    </div>
 </template>
+
+<style scoped>
+.header {
+    font-size: 32px;
+    color: #00bfff;
+}
+.logo {
+    width: 125px;
+    height: 94px;
+}
+</style>
--- sources/SearchEngine-FrontEnd/src/components/Home.vue
+++ sources/SearchEngine-FrontEnd/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="wrap">
         <div class="searchbox">
             <el-input v-model="searchkey">
                 <el-button type="primary" @click="link" slot="append">搜索</el-button>
@@ -23,3 +23,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.wrap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: url(../assets/background.jpg);
+  background-size: 100%;
+  padding: 0px;
+}
+.searchbox {
+  position: absolute;
+  left: 25%;
+  top: 50%;
+  width: 50%;
+}
+</style>
